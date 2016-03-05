aacce87f9829b6024b8f444710cf14203c816d57
Survingo <thehydra.tk@gmail.com>
2016-03-05 14:27:30 +0100

    Update BedWars.php

--- src/BedWarsPE/BedWars.php
+++ src/BedWarsPE/BedWars.php
@@ -1,1 +1,9 @@
-soon
+<?php
+namespace BedWarsPE;
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\item\Item;
+use pocketmine\event\EventListener;
+use pocketmine\Player;
+use pocketmine\block\Block;
